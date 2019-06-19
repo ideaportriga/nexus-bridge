49a69d5f0f5e8d2ae0791673a654fde3bf8d8119
OlegKoleskin <oleg.koleskin@gmail.com>
2019-06-19 14:31:32 +0300

    att implemented as vue

--- packages/nexus19/PUBLIC/AccountAttachmentListAppletPR.js
+++ packages/nexus19/PUBLIC/AccountAttachmentListAppletPR.js
@@ -5,9 +5,7 @@ if (typeof (SiebelAppFacade.AccountAttachmentListAppletPR) === "undefined") {
     function () {
       SiebelAppFacade.AccountAttachmentListAppletPR = (function () {
         var keepOUI = false;
-        var pm;
-
-
+        var _pm;
 
         function AccountAttachmentListAppletPR(pm) {
           SiebelAppFacade.AccountAttachmentListAppletPR.superclass.constructor.apply(this, arguments);
@@ -24,7 +22,7 @@ if (typeof (SiebelAppFacade.AccountAttachmentListAppletPR) === "undefined") {
             $('head').append('<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet"></link>');
             $('head').append('<link type="text/css"  rel="stylesheet" href="files/custom/vuetify.min.css"/>');
             $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">');
-            pm = this.GetPM();
+            _pm = this.GetPM();
           }
         }
 
@@ -32,8 +30,9 @@ if (typeof (SiebelAppFacade.AccountAttachmentListAppletPR) === "undefined") {
           if (keepOUI) {
             SiebelAppFacade.AccountAttachmentListAppletPR.superclass.ShowUI.apply(this, arguments);
           } else {
-            putPlugin("s_" + this.GetPM().Get('GetFullId') + "_div");
-
+            var divId = "s_" + this.GetPM().Get('GetFullId') + "_div";
+            $('#' + divId).replaceWith("<div id='app'>This is a upload area<input type='file' id='fileupload' name='test'></div>"); // SWE_FILE_NAME_STR
+            putVue();
           }
         }
 
@@ -43,71 +42,79 @@ if (typeof (SiebelAppFacade.AccountAttachmentListAppletPR) === "undefined") {
           }
         }
 
-
         AccountAttachmentListAppletPR.prototype.BindData = function (bRefresh) {
           if (keepOUI) {
             SiebelAppFacade.AccountAttachmentListAppletPR.superclass.BindData.apply(this, arguments);
           }
         }
 
-        function upload(event, n) {
-          console.log('upload started....');
-          if (n.files.length > 0) {
-            var $element = $('#fileupload');
-            var newFileAttRetValue = pm.ExecuteMethod('NewFileAttachment');
-            console.log(newFileAttRetValue);
-            var srnString = consts.get("SWE_PROP_SESSION_RANDOM_NUMBER") + "=" + SiebelApp.S_App.GetSRN();
-            $element.fileupload("option", "remainingFilesToUpload", n.files.length);
-            $element.fileupload("option", "formData", newFileAttRetValue);
-            $element.fileupload("option", "url", SiebelApp.S_App.GetPageURL() + consts.get("SWE_ARG_START") + srnString);
-          }
-        }
-
-        function uploadDone(event, data) {
-          var $element = $('#fileupload');
-          var result = data.result;
-          var remainingFiles = 0;
-          console.log('upload done...', result);
-
-          if ($element.data("blueimp-fileupload")) {
-            remainingFiles = $element.fileupload("option", "remainingFilesToUpload");
-            $element.fileupload("option", "remainingFilesToUpload", --remainingFiles);
-          }
-          if (!utils.IsEmpty(result)) {
-            SiebelApp.S_App.ProcessResponse(result).done(function () {
-              SiebelApp.S_App.ProcessError();
-              pm.ExecuteMethod('InvokeMethod', 'WriteRecord');
-              console.log(pm.Get('GetRecordSet'));
-            });
-          }
-          if (remainingFiles <= 0) {
-            $element.val("");
-          }
-        }
-
-        function putPlugin(divId) {
-          $('#' + divId).replaceWith("<div id='test1'>This is a upload area<input type='file' id='fileupload' name='test'></div>"); // SWE_FILE_NAME_STR
-          var $element = $('#fileupload');
-
-          $element.fileupload({
-            dropZone: $("#s_" + pm.Get('GetFullId') + "_div"),
-            fileInput: $element,
-            forceIframeTransport: false,
-            formData: {},
-            multipart: true,
-            replaceFileInput: false,
-            paramName: 's_SweFileName', // Server relies on this name
-            // scope: i
-            type: "POST",
-            url: SiebelApp.S_App.GetPageURL(),
-            fail: function () {
-              console.log('upload fail...', arguments);
+        function putVue() {
+          new Vue({
+            el: '#app',
+            mounted() {
+              this.initializePlugin();
             },
-            done: uploadDone
-          }).bind("fileuploadchange", { ctx: this }, function (event, data) {
-            return upload.call(this, event, data);
-          }).bind("fileuploaddrop", { ctx: this }, function (event, data) {
-            return upload.call(this, event, data);
+            methods: {
+              initializePlugin() {
+                var $element = $('#fileupload');
+                var vm = this;
+
+                $element.fileupload({
+                  dropZone: $("#s_" + _pm.Get('GetFullId') + "_div"),
+                  fileInput: $element,
+                  forceIframeTransport: false,
+                  formData: {},
+                  multipart: true,
+                  replaceFileInput: false,
+                  paramName: 's_SweFileName', // Server relies on this name
+                  // scope: i
+                  type: "POST",
+                  url: SiebelApp.S_App.GetPageURL(),
+                  fail: function () {
+                    console.log('upload fail...', arguments);
+                  },
+                  done: this.uploadDone
+                }).bind("fileuploadchange", { ctx: this }, function (event, data) {
+                  return vm.upload.call(null, event, data);
+                }).bind("fileuploaddrop", { ctx: this }, function (event, data) {
+                  return vm.upload.call(null, event, data);
+                });
+              },
+              uploadDone(event, data) {
+                var $element = $('#fileupload');
+                var result = data.result;
+                var remainingFiles = 0;
+                console.log('upload done...', result);
+
+                if ($element.data("blueimp-fileupload")) {
+                  remainingFiles = $element.fileupload("option", "remainingFilesToUpload");
+                  $element.fileupload("option", "remainingFilesToUpload", --remainingFiles);
+                }
+                if (!utils.IsEmpty(result)) {
+                  SiebelApp.S_App.ProcessResponse(result).done(function () {
+                    SiebelApp.S_App.ProcessError();
+                    _pm.ExecuteMethod('InvokeMethod', 'WriteRecord');
+                    console.log(_pm.Get('GetRecordSet'));
+                  });
+                }
+                if (remainingFiles <= 0) {
+                  $element.val("");
+                }
+              },
+              upload(event, data) {
+                console.log('upload started....');
+                if (data.files.length > 0) {
+                  var $element = $('#fileupload');
+                  var newFileAttRetValue = _pm.ExecuteMethod('NewFileAttachment');
+                  console.log(_pm);
+                  console.log(newFileAttRetValue);
+                  var srnString = consts.get("SWE_PROP_SESSION_RANDOM_NUMBER") + "=" + SiebelApp.S_App.GetSRN();
+                  $element.fileupload("option", "remainingFilesToUpload", data.files.length);
+                  $element.fileupload("option", "formData", newFileAttRetValue);
+                  $element.fileupload("option", "url", SiebelApp.S_App.GetPageURL() + consts.get("SWE_ARG_START") + srnString);
+                }
+              }
+            }
           });
         }
 

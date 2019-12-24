2a6ec052b22ed025614d2e615b2fd6ec75219ac9
Andrejs Agejevs <esneko@gmail.com>
2019-12-24 18:44:14 +0200

    Nexus Factory API to create popup applets

--- packages/nexus-factory/src/index.ts
+++ packages/nexus-factory/src/index.ts
@@ -1,12 +1,38 @@
 import Nexus from '@ideaportriga/nexus-bridge'
-import { NexusConfig, NexusBridge } from './types'
+import { NexusBridge, NexusConfig } from './types'
 
 declare const window: any
 
-const memo: any = {}
+const memo: Record<string, NexusBridge> = {}
+
+const NexusPopup = (appletName: string, pm: any): NexusBridge => {
+  const isPopup = pm.Get('IsPopup')
+
+  // popup applet PM
+  const popupPM = window.SiebelApp.S_App.GetPopupPM()
+
+  // if assoc exists
+  const assocApplet = popupPM.Get('MVGAssocAppletObject')
+  const isShuttle = popupPM.Get('isPopupMVGAssoc')
+  const isMvgAssoc =
+    isShuttle && assocApplet ? appletName === assocApplet.GetName() : false
+
+  return Nexus.CreatePopupNB({
+    pm,
+    isPopup,
+    isMvgAssoc,
+    convertDates: true
+  })
+}
 
 const NexusFactory = (config: string | NexusConfig): null | NexusBridge => {
-  if (config !== null && typeof config === 'object') {
+  if (typeof config === 'string') {
+    const key = config || 'default'
+
+    return memo[key]
+  }
+
+  if (typeof config === 'object') {
     for (const key in memo) {
       console.log(`[NF] Nexus instance deleted: ${memo[key].appletName}`)
       delete memo[key]
@@ -14,28 +40,29 @@ const NexusFactory = (config: string | NexusConfig): null | NexusBridge => {
 
     for (const key in config) {
       const appletName = config[key]
-      const pm = window.SiebelApp.S_App.GetActiveView()
-        .GetApplet(appletName)
-        .GetPModel()
+      const applet = window.SiebelApp.S_App.GetActiveView().GetApplet(
+        appletName
+      )
+      if (!applet) {
+        console.log(`[NF] Applet not found: ${appletName}`)
+      }
 
-      memo[key] = new Nexus({
-        pm,
-        convertDates: true
-      })
+      const pm = applet.GetPModel()
+      const isPopup = pm.Get('IsPopup')
+      if (isPopup) {
+        memo[key] = NexusPopup(appletName, pm)
+      } else {
+        memo[key] = new Nexus({
+          pm,
+          convertDates: true
+        })
+      }
 
       console.log(`[NF] Nexus instance created: ${memo[key].appletName}`)
     }
-
-    return null
   }
 
-  if (typeof config === 'string' && config.length !== 0) {
-    const key = config
-
-    return memo[key]
-  }
-
-  return null
+  return memo
 }
 
-export { NexusConfig, NexusBridge, NexusFactory }
+export { NexusBridge, NexusConfig, NexusFactory, NexusPopup }
